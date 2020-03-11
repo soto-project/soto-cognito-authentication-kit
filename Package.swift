626331dfb1a17aadeaf061de2dd30ee628746be4
Adam Fowler <adamfowler71@gmail.com>
2020-03-11 17:44:26 +0000

    Setup for Vapor 4 release candidate
    Update jwt-kit version to 4.0.0-rc
    Frustratingly this forces us to move up to Swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,19 +13,19 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.0.0-beta.2.1")),
+        .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.0.0-rc")),
         // for SRP
         .package(url: "https://github.com/adam-fowler/big-num.git", .upToNextMajor(from: "1.1.0")),
     ],
     targets: [
         .target(name: "AWSCognitoAuthenticationKit",
                 dependencies: [
-                    "AsyncHTTPClient",
-                    "BigNum",
-                    "CognitoIdentity",
-                    "CognitoIdentityProvider",
-                    "JWTKit",
-                    "Crypto"
+                    .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                    .product(name: "BigNum", package: "big-num"),
+                    .product(name: "CognitoIdentity", package: "aws-sdk-swift"),
+                    .product(name: "CognitoIdentityProvider", package: "aws-sdk-swift"),
+                    .product(name: "JWTKit", package: "jwt-kit"),
+                    .product(name: "Crypto", package: "swift-crypto")
             ]
         ),
         .testTarget(name: "AWSCognitoAuthenticationKitTests", dependencies: ["AWSCognitoAuthenticationKit"]),
