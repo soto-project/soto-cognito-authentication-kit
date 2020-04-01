43ae231139d3cb2f8d3e61aa216e29674465281b
Adam Fowler <adamfowler71@gmail.com>
2020-04-01 08:30:34 +0100

    Update Package to work with package names

--- Package.swift
+++ Package.swift
@@ -12,18 +12,18 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", .upToNextMajor(from: "4.0.0")),
+        .package(name: "AWSSDKSwift", url: "https://github.com/swift-aws/aws-sdk-swift.git", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.0.0-rc")),
         // for SRP
-        .package(url: "https://github.com/adam-fowler/big-num.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/adam-fowler/big-num.git", .upToNextMajor(from: "1.1.1")),
     ],
     targets: [
         .target(name: "AWSCognitoAuthenticationKit",
                 dependencies: [
                     .product(name: "AsyncHTTPClient", package: "async-http-client"),
                     .product(name: "BigNum", package: "big-num"),
-                    .product(name: "CognitoIdentity", package: "aws-sdk-swift"),
-                    .product(name: "CognitoIdentityProvider", package: "aws-sdk-swift"),
+                    .product(name: "CognitoIdentity", package: "AWSSDKSwift"),
+                    .product(name: "CognitoIdentityProvider", package: "AWSSDKSwift"),
                     .product(name: "JWTKit", package: "jwt-kit"),
                     .product(name: "Crypto", package: "swift-crypto")
             ]
