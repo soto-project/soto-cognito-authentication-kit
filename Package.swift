f57c1bc92a13457b841ac8cd70acf4ad32c4842f
Adam Fowler <adamfowler71@gmail.com>
2020-09-26 12:11:57 +0100

    Use 5.0.0-beta

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/soto-project/soto.git", .branch("main")),
+        .package(url: "https://github.com/soto-project/soto.git", .upToNextMajor(from: "5.0.0-beta")),
         .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.0.0")),
         // for SRP
         .package(url: "https://github.com/adam-fowler/big-num.git", .upToNextMajor(from: "2.0.0")),
