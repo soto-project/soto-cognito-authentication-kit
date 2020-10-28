9d32ac6dae0764f36c6b35e954bafbe909da92f8
Adam Fowler <adamfowler71@gmail.com>
2020-10-28 07:25:06 +0000

    Add iOS platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "aws-cognito-authentication-kit",
-    platforms: [.macOS(.v10_15)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         .library(name: "AWSCognitoAuthenticationKit", targets: ["AWSCognitoAuthenticationKit"]),
         .library(name: "AWSCognitoAuthenticationSRP", targets: ["AWSCognitoAuthenticationSRP"]),
