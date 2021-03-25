76ecaafe19a3e5e3f7e4d2e26be6cc293e624e06
Adam Fowler <adamfowler71@gmail.com>
2021-03-25 12:38:04 +0000

    File headers and LICENSE

--- Sources/SotoCognitoAuthenticationKit/ContextData.swift
+++ Sources/SotoCognitoAuthenticationKit/ContextData.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Soto for AWS open source project
+//
+// Copyright (c) 2020-2021 the Soto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Soto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import SotoCognitoIdentityProvider
 
 /// Protocol for objects that contains context data to be used by Cognito
