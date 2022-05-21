319db7851081c2881a4f7427cfc8b1b0caff31af
Adam Fowler <adamfowler71@gmail.com>
2022-05-21 10:43:59 +0100

    Dependent on Soto 6.0.0-rc

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/soto-project/soto.git", .branch("main")),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.6.0"),
+        .package(url: "https://github.com/soto-project/soto.git", from: "6.0.0-rc"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.10.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.2.6")),
         // for SRP
         .package(url: "https://github.com/adam-fowler/big-num.git", .upToNextMajor(from: "2.0.0")),
