dceb86aa65ffb9d3ed9d9201601ed29f5d421d74
Adam Fowler <adamfowler71@gmail.com>
2024-06-08 16:42:44 +0100

    Loosen swift-crypto version requirements

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         .library(name: "SotoCognitoAuthenticationSRP", targets: ["SotoCognitoAuthenticationSRP"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
         .package(url: "https://github.com/soto-project/soto.git", from: "7.0.0-beta"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.10.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.2.6")),
