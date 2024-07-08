435ac6290c8ebbdb30fa2a94969a2f96cf5eaad4
Adam Fowler <adamfowler71@gmail.com>
2024-07-08 13:22:54 +0100

    Use jwt-kit 5.0.0-rc.1

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
         .package(url: "https://github.com/soto-project/soto.git", from: "7.0.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.10.0"),
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0-beta.4"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0-rc.1"),
         // for SRP
         .package(url: "https://github.com/adam-fowler/big-num.git", .upToNextMajor(from: "2.0.0")),
     ],
