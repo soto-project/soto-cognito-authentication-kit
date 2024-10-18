fa415351e5a87f1fa05f976cd98b3bd4230430ef
Adam Fowler <adamfowler71@gmail.com>
2024-10-18 07:40:03 +0100

    Use soto 7.1.0

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"5.0.0"),
-        .package(url: "https://github.com/soto-project/soto.git", from: "7.0.0"),
+        .package(url: "https://github.com/soto-project/soto.git", from: "7.1.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.10.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0"),
         // for SRP
